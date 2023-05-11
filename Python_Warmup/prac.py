98294bb785addf51f2757b6983f3ae56f20cfa96
Shyam Goli <golishyamgolisai@gmail.com>
2023-05-11 17:18:59 +0530

    added prac.py

--- Python_Warmup/prac.py
+++ Python_Warmup/prac.py
@@ -11,3 +11,28 @@
 # # print()
 
 # print(type(st))
+
+# Pallindrome code
+# n = input("Enter the number : ")
+# st = ''
+# for i in reversed(n):
+#     st += i
+# if n == st:
+#     print("It is a pallindrome number")
+# else:
+#     print("It is not a pallindrome number")
+
+# n,m = list(map(int,input("Enter the range : ").split()))
+# list_prime = []
+# for i in range(n,m+1):
+#     prime = True
+#     for j in range(2,(i//2)+1):
+#         if i % j == 0:
+#             prime = False
+#             break
+#     if prime:
+#         list_prime.append(i)
+# print(f"List of prime numbers"list_prime)
+
+
+
