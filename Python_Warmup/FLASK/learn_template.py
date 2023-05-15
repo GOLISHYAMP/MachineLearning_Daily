be4742e33fc247aea706b638aedb55b37c076e1c
Shyam Goli <golishyamgolisai@gmail.com>
2023-05-15 16:24:24 +0530

    work in progress in flask

--- Python_Warmup/FLASK/learn_template.py
+++ Python_Warmup/FLASK/learn_template.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,redirect
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("home.html")
 
+@app.route('/courses')
+def courses():
+    li = ['AI','DL','ML','CV']
+    content = 'HELLO I AM THE CONTENT'
+    return render_template('courses.html',context=[content,li])
+
+@app.route("/admin")
+def admin():
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
