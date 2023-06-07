036c975cd11753c4d544742d1a2d4ada029132df
Shyam Goli <golishyamgolisai@gmail.com>
2023-06-07 17:53:16 +0530

    some changes

--- Python_Warmup/FLASK/Sessions/app.py
+++ Python_Warmup/FLASK/Sessions/app.py
@@ -1,6 +1,8 @@
 from flask import Flask,render_template,redirect,url_for,session,request
 
 app = Flask(__name__)
+app.secret_key = "shyam"
+# session.permanent = True
 
 @app.route('/')
 def home():
@@ -10,12 +12,17 @@ def home():
 def login():
     if request.method == "POST":
         username = request.form["username"]
-        return redirect(url_for("user",user= username))
+        session['user'] = username
+        return redirect(url_for("user"))
     return render_template("login.html")
 
-@app.route('/<user>')
-def user(user):
-    return f"Your login is successfull {user}"
+@app.route('/user')
+def user():
+    if 'user' in session:
+        name = session['user']
+        return f"Your login is successfull {name}"
+    else:
+        return render_template("login.html")
 
 
 
